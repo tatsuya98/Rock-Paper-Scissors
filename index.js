af57f30eee428c92902ccd16521655e09e4ca392
tatsuya98 <nathanbailey_62@hotmail.com>
2022-08-26 15:39:14 +0100

    completed the logic check for rock player choice

--- index.js
+++ index.js
@@ -9,7 +9,26 @@ const getUserChoice = ()=>{
     const userChoice = prompt('choose from the three options: rock, paper, scissors','');
     return userChoice
 }
+const playGame = ()=>{
+    const userChoice = getUserChoice();
+    const computerChoice = getComputerChoice();
+    const win = `the computer's choice was: ${computerChoice}. you win`
+    const lose = `the computer's choice was: ${computerChoice}. you lose`
+    const tie = `the computer's choice was: ${computerChoice}. you drew`
+    let result = ''
+    if(userChoice.toLowerCase() == 'rock' && computerChoice.toLowerCase() == 'rock'.toLowerCase()){
+        result = tie;
+        console.log(result)
+    }else if(userChoice.toLowerCase() == 'rock' && computerChoice.toLowerCase() == 'paper'.toLowerCase()){
+        result = lose
+        console.log(result)
+    }else if(userChoice.toLowerCase() == 'rock' && computerChoice.toLowerCase() == 'scissors'.toLowerCase()){
+        result = win;
+        console.log(result);
+    }
+}
 
+playGame();
 
 
 
