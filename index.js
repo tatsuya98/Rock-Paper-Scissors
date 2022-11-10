d52cd269ef071a085cee51799a780b3fb2f056be
tatsuya98 <nathanbailey_62@hotmail.com>
2022-11-10 01:24:29 +0000

    added a score for user and computer added buttons with event listeners created 2 funtions for playing and setting up the game

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
+
+const h1 = document.querySelector('h1')
+const h2 = document.querySelector('h2')
+const body = document.querySelector('body')
+const play = document.querySelector('#play')
+const rock = document.createElement('button')
+const paper = document.createElement('button')
+const scissors = document.createElement('button')
+
+const rockImage = document.createElement('img')
+const paperImage = document.createElement('img')
+const scissorsImage = document.createElement('img')
+
+rockImage.style.height = '100px'
+rockImage.style.width = '100px'
+paperImage.style.height = '100px'
+paperImage.style.width = '100px'
+scissorsImage.style.height = '100px'
+scissorsImage.style.width = '100px'
+const rockSrc = document.createAttribute('src')
+rockSrc.value = 'https://thumbs.dreamstime.com/b/cartoon-rock-pebbles-digital-illustration-147192308.jpg'
+rockImage.setAttributeNode(rockSrc)
+const paperSrc = document.createAttribute('src')
+paperSrc.value = 'https://i.pinimg.com/736x/4f/ae/cf/4faecfda18947d8e03ef0d188e4de42d.jpg'
+paperImage.setAttributeNode(paperSrc)
+const scissorsSrc = document.createAttribute('src')
+scissorsSrc.value = 'https://static.vecteezy.com/system/resources/previews/004/557/738/original/black-scissors-cartoon-object-vector.jpg'
+scissorsImage.setAttributeNode(scissorsSrc)
+
+
+rock.setAttribute('value','rock')
+paper.setAttribute('value','paper')
+scissors.setAttribute('value','scissors')
+
+rock.setAttribute('class','choice')
+paper.setAttribute('class','choice')
+scissors.setAttribute('class','choice')
+
+
+rock.appendChild(rockImage)
+paper.appendChild(paperImage)
+scissors.appendChild(scissorsImage)
+
+
+
 const getComputerChoice = ()=>{
     const randomNummber = Math.floor(Math.random()*3);
     const choiceArray = ['Rock', 'Paper', 'Scissors']
@@ -5,79 +50,69 @@ const getComputerChoice = ()=>{
     return computerChoice
 }
 
-const getUserChoice = ()=>{
-    let userChoice = prompt('choose from the three options: rock, paper, scissors','');
-    if(userChoice == 'rock'.toLowerCase() || userChoice == 'paper'.toLowerCase() || userChoice == 'scissors'.toLowerCase()){
-        return userChoice
-    }
-     userChoice = prompt('you have entered a wrong choice. choose again from rock paper or scissors','')
-     if(userChoice == 'rock'.toLowerCase() || userChoice == 'paper'.toLowerCase() || userChoice == 'scissors'.toLowerCase()){
-        return userChoice
-     }
-}
-const playGame = ()=>{
-    let userWinCount = 0;
-    let computerWinCount = 0;
-    const numberOfRounds = Number.parseInt(prompt('enter the number of rounds you wish to play',''))
+let roundsPlayed 
+let userWinCount 
+let computerWinCount 
+let userChoice
+let numberOfRounds
 
-    for(let i =0; i < numberOfRounds; i++){
-        alert(`round ${i+1}`)
-        let userChoice = getUserChoice();
-        while(!userChoice){
-            userChoice=getUserChoice()
-        }
-        const computerChoice = getComputerChoice();
-        const win = `the computer's choice was: ${computerChoice}. you win the round`
-        const lose = `the computer's choice was: ${computerChoice}. you lose the round`
-        const tie = `the computer's choice was: ${computerChoice}. you drew the round`
-        if(userChoice.toLowerCase() == 'rock' && computerChoice.toLowerCase() == 'rock'.toLowerCase()){
-            console.log(tie)
-            console.log(`the current score is user:${userWinCount} : computer: ${computerWinCount}`)
-        }else if(userChoice.toLowerCase() == 'rock' && computerChoice.toLowerCase() == 'paper'.toLowerCase()){
-            result = lose
-            computerWinCount +=1
-            console.log(lose)
-            console.log(`the current score is user:${userWinCount} : computer: ${computerWinCount}`)
-        }else if(userChoice.toLowerCase() == 'rock' && computerChoice.toLowerCase() == 'scissors'.toLowerCase()){
-            result = win;
-            userWinCount +=1
-            console.log(win);
-            console.log(`the current score is user:${userWinCount} : computer: ${computerWinCount}`)
-        }
-        if(userChoice.toLowerCase() == 'paper' && computerChoice.toLowerCase() == 'rock'.toLowerCase()){
-            userWinCount+=1
-            console.log(win)
-            console.log(`the current score is user:${userWinCount} : computer: ${computerWinCount}`)
-        }else if(userChoice.toLowerCase() == 'paper' && computerChoice.toLowerCase() == 'paper'.toLowerCase()){
-            console.log(tie)
-            console.log(`the current score is user:${userWinCount} : computer: ${computerWinCount}`)
-        }else if(userChoice.toLowerCase() == 'paper' && computerChoice.toLowerCase() == 'scissors'.toLowerCase()){
-            computerWinCount+=1
-            console.log(lose);
-            console.log(`the current score is user:${userWinCount} : computer: ${computerWinCount}`)
-        }
-        if(userChoice.toLowerCase() == 'scissors' && computerChoice.toLowerCase() == 'rock'.toLowerCase()){
-            computerWinCount+=1
-            console.log(lose)
-            console.log(`the current score is user:${userWinCount} : computer: ${computerWinCount}`)
-        }else if(userChoice.toLowerCase() == 'scissors' && computerChoice.toLowerCase() == 'paper'.toLowerCase()){
-            userWinCount+=1
-            console.log(win)
-            console.log(`the current score is user:${userWinCount} : computer: ${computerWinCount}`)
-        }else if(userChoice.toLowerCase() == 'scissors' && computerChoice.toLowerCase() == 'scissors'.toLowerCase()){
-            console.log(tie);
-            console.log(`the current score is user:${userWinCount} : computer: ${computerWinCount}`);
-        }
-    }
-    if(userWinCount > computerWinCount){
-        console.log(`You beat the computer with a score of ${userWinCount} to ${computerWinCount} `)
-    }else if(userWinCount == computerWinCount){
-        console.log(`You tied against the computer with a score of ${userWinCount} to ${computerWinCount}`)
-    }else{
-        console.log(`The computer wins with a score of ${computerWinCount} to ${userWinCount}`)
-    }
+const setupGame = () =>{
+    
+    roundsPlayed =1;
+    userWinCount = 0;
+    computerWinCount = 0;
+    body.removeChild(play)
+    h1.textContent = `user : ${userWinCount} : computer: ${computerWinCount}`
+    body.append(rock,paper,scissors)
+    numberOfRounds = Number.parseInt(prompt('enter the number of rounds you wish to play',''))
+    const choices = document.querySelectorAll('.choice')
+    choices.forEach(choice=>{
+        choice.addEventListener('click',playGame)
+    })
 }
-playGame()
+
+const playGame = (e)=>{
+    const choices = document.querySelectorAll('.choice')
+    userChoice = e.currentTarget.value
+    const computerChoice = getComputerChoice();
+    if(userChoice == 'rock'.toLowerCase() && computerChoice == 'rock'.toLowerCase() ||
+        userChoice == 'paper'.toLowerCase() && computerChoice == 'paper'.toLowerCase() ||
+        userChoice == 'scissors'.toLowerCase() && computerChoice == 'scissors'.toLowerCase() ){
+            roundsPlayed++
+            h1.textContent = `user : ${userWinCount} : computer: ${computerWinCount}`
+        }else if(userChoice.toLowerCase() == 'rock' && computerChoice.toLowerCase() == 'paper'.toLowerCase() ||
+        userChoice.toLowerCase() == 'paper' && computerChoice.toLowerCase() == 'scissors'.toLowerCase() ||
+        userChoice.toLowerCase() == 'scissors' && computerChoice.toLowerCase() == 'rock'.toLowerCase()){
+            computerWinCount++
+            roundsPlayed++
+            h1.textContent = `user : ${userWinCount} : computer: ${computerWinCount}`
+        }else{
+            userWinCount++
+            roundsPlayed++
+            h1.textContent = `user : ${userWinCount} : computer: ${computerWinCount}`
+        }
+        if(roundsPlayed > numberOfRounds){
+            if(userWinCount > computerWinCount){
+                h1.textContent = `You beat the computer with a score of ${userWinCount} to ${computerWinCount} `
+            }else if(userWinCount == computerWinCount){
+                h1.textContent = `You tied against the computer with a score of ${userWinCount} to ${computerWinCount}`
+            }else{
+                h1.textContent = `The computer wins with a score of ${computerWinCount} to ${userWinCount}`
+            }
+            body.removeChild(rock)
+            body.removeChild(paper)
+            body.removeChild(scissors)
+            choices.forEach(choice =>{
+                choice.removeEventListener('click',playGame)
+            })
+            play.textContent = 'play again'
+            body.appendChild(play)
+        }
+        
+    }
+play.addEventListener('click',setupGame)
+
+
 
     
 
